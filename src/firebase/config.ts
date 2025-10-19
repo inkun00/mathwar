d2e23a9cecd0a79ad3c154b19c849bfed7d65a87
Inkeun Yoo <inkun00@gmail.com>
2025-10-19 04:08:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4630974581-a7bbe",
+  "appId": "1:478126188439:web:2e96b9f9673a4e915a86d2",
+  "apiKey": "AIzaSyCVVTf2V5vortw0avyytgdCLoZL5CE2fZY",
+  "authDomain": "studio-4630974581-a7bbe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "478126188439"
+};
