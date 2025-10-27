c169c1d0bc7ce40a9ac9779231cf0fadc22c4c2b
Inkeun Yoo <inkun00@gmail.com>
2025-10-27 04:52:00 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/profile-sheet.tsx
+++ src/components/profile-sheet.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from "react";
 import type { User, Country, ProblemAttempt, ProblemSubType, WrongAnswer, StorableProblem, Tile, RankedUser, RankedCountry } from "@/lib/types";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
