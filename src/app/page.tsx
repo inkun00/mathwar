2c8b04040afde3f2dfa9e7d8a699d59bb58f11ab
Inkeun Yoo <inkun00@gmail.com>
2025-10-30 05:10:38 +0000

    Runtime Error
    FirebaseError: Missing or insufficient permissions: The

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 
   // user is logged in, has a profile, and all data is loaded
   // problemAttempts and wrongAnswers can be null for new users.
-  if (userProfile && countries && landTiles && problemAttempts !== undefined && wrongAnswers !== undefined && allUsers) {
+  if (userProfile && countries && landTiles && allUsers && problemAttempts !== undefined && wrongAnswers !== undefined) {
     return (
       <div className="relative flex h-screen w-full flex-col items-center bg-background p-4 sm:p-6 md:p-8">
         <GameBoard 
