b1e6976181f8729cbf8d7f91bbaa54fc4dd26a62
Inkeun Yoo <inkun00@gmail.com>
2025-10-30 03:40:11 +0000

    Try fixing this error: `Console Error: [2025-10-30T03:39:33.407Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,15 +8,7 @@ import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    let firebaseApp;
-    try {
-      firebaseApp = initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
+    const firebaseApp = initializeApp(firebaseConfig);
     
     const firestore = getFirestore(firebaseApp);
     // Enable offline persistence
