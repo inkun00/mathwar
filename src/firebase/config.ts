13f8f81a6ef9bc02088366ece84c73e5c17750d2
Inkeun Yoo <inkun00@gmail.com>
2025-11-03 03:18:59 +0000

    Try fixing this error: `Console Error: [2025-11-03T03:18:20.113Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  "projectId": "studio-4630974581-a7bbe",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-4630974581-a7bbe",
   "appId": "1:478126188439:web:2e96b9f9673a4e915a86d2",
   "apiKey": "AIzaSyCVVTf2V5vortw0avyytgdCLoZL5CE2fZY",
   "authDomain": "studio-4630974581-a7bbe.firebaseapp.com",
